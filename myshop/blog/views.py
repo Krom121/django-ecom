ea848b90c75da633f838b8835a09b778b4680dd9
krom121 <stephenacgregor9@gmail.com>
2019-04-24 14:37:25 +0100

    class view for post list+css

--- myshop/blog/views.py
+++ myshop/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from django.views.generic import ListView
 
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog_list.html', {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog_list.html'
 
 
 def post_detail(request, year, month, day, post):
